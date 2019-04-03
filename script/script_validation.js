84c3a4118bb7aaadaa4065f9cd13542d58b30783
dldisha <dldisha929@gmail.com>
2019-04-03 16:07:05 +0530

    initialize

---
+++ script/script_validation.js
@@ -0,0 +1,12 @@
+	//shows form on click
+		function myFunction() {
+  var x = document.getElementById("contacts-form");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+//form validations
+// Wait for the DOM to be ready
