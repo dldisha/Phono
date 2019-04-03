dfe8471d0d12050b2d6fad6b5fdc9d49626eec45
dldisha <dldisha929@gmail.com>
2019-04-03 17:24:21 +0530

    search added

--- script/script_validation.js
+++ script/script_validation.js
@@ -8,5 +8,41 @@
   }
 }
 
-//form validations
-// Wait for the DOM to be ready
+		function search() {
+  var x = document.getElementById("search-form");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+var searchForm = document.getElementById('search-form');
+searchForm.addEventListener('submit', function(){
+  var results;
+  if(results !== undefined){
+    results = null;
+  }
+  if(!window.contactList){
+    window.contactList = $ab();
+  }else{
+    results = contactList.search(searchForm.search.value);
+  }
+  document.getElementById('results').innerHTML = '';
+  if(results.length>0){
+    
+    for(var i = 0;i<results.length;i++){
+      document.getElementById('results').innerHTML += '<div class="contact-item">Name:'+results[i].first_name+'<br>Phone:'+results[i].phone+'</div><hr>';
+    }
+  } else{
+   document.getElementById('results').innerHTML += '<div class="contact-item">There are no results for this name</div><hr>';
+  }
+  
+  //do something with the results
+  event.preventDefault();
+});
+
+
+
+
+
